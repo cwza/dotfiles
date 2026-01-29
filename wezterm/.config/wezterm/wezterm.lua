6dc760f8f24d5dba73c048ca6693a217cbfe9125
cwza <cwz0205a@gmail.com>
2026-01-29 10:33:02 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,10 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
--- window size
-config.initial_rows = 20
-config.initial_cols = 100
-
 -- font
 config.font = wezterm.font("MesloLGL Nerd Font")
 config.font_size = 24
