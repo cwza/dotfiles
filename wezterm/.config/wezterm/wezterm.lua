cacd85fc3fc698935ba96038607d6fdde9d262a1
cwza <cwz0205a@gmail.com>
2026-02-01 22:11:32 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,19 +5,4 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGL Nerd Font")
 config.font_size = 24
 
--- key
-config.keys = {
-    -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
-    {
-        key = 'LeftArrow',
-        mods = 'OPT',
-        action = wezterm.action.SendKey { key = 'b', mods = 'ALT' },
-    },
-    {
-        key = 'RightArrow',
-        mods = 'OPT',
-        action = wezterm.action.SendKey { key = 'f', mods = 'ALT' },
-    },
-}
-
 return config
