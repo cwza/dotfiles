a2e849e1a32b868421fd1bfdc9ea24fe3ddf74bd
cwza <cwz0205a@gmail.com>
2026-03-18 17:50:37 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,4 +5,12 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGL Nerd Font")
 config.font_size = 24
 
+-- keys
+config.keys = {
+    {
+        key = "F13",
+        action = wezterm.action.SendKey { key = "a", mods = "CTRL" },
+    },
+}
+
 return config
