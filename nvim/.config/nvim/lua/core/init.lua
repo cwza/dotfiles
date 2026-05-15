df1e5c84a7228016ce9d7bd79b50f1ed02ba1ac2
cwza <cwz0205a@gmail.com>
2026-05-15 17:22:32 +0800

    update

--- nvim/.config/nvim/lua/core/init.lua
+++ nvim/.config/nvim/lua/core/init.lua
@@ -2,3 +2,21 @@ require("core.keymaps")
 require("core.options")
 require("core.lsp")
 require("core.snippet")
+
+vim.api.nvim_create_autocmd("FileType", {
+    desc = "Enable Treesitter features",
+    callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if not lang then return end
+        if vim.treesitter.query.get(lang, "highlights") then
+            vim.treesitter.start(args.buf)
+        end
+        -- if vim.treesitter.query.get(lang, "indents") then
+        --     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        -- end
+        if vim.treesitter.query.get(lang, "folds") then
+            vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo[0][0].foldmethod = "expr"
+        end
+    end,
+})
